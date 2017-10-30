9b297ecaf1fdfd6b734ac69b60cf3fdb0cbf4c73
Andhitia Rama <andhitia.r@gmail.com>
2017-10-30 10:38:24 +0700

    [purchase_operating_unit] Fix lint

--- purchase_request_operating_unit/__openerp__.py
+++ purchase_request_operating_unit/__openerp__.py
@@ -1,102 +1,20 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2014 Eficent (<http://www.eficent.com/>)
-#               <contact@eficent.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 Eficent Business and IT Consulting Services S.L. -
+# Jordi Ballester Alomar
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Operating Unit in Purchase Requests",
-    "version": "1.0",
-    "author": "Eficent",
+    "version": "8.0.1.0.0",
+    "author": "Eficent, Odoo Community Association (OCA)",
     "website": "http://www.eficent.com",
     "category": "Purchase Management",
     "depends": ["purchase_request",
                 "purchase_operating_unit"],
-    "description": """
-Operating Unit in Purchase Requests
-===================================
-This module was written to extend the Purchase capabilities of Odoo.
-
-This module introduces the operating unit to the purchase requests.
-
-Security rules are defined to ensure that users can only display the
-Purchase Requests in which they are allowed access to.
-
-Installation
-============
-
-No additional installation instructions are required.
-
-
-Configuration
-=============
-
-This module does not require any additional configuration.
-
-Usage
-=====
-
-At the time when a user creates a new purchase request the system
-proposes the user's default operating unit.
-
-The operating unit is a required field.
-
-Known issues / Roadmap
-======================
-
-No issue has been identified.
-
-Credits
-=======
-
-Contributors
-------------
-
-* Jordi Ballester <jordi.ballester@eficent.com>
-
-Maintainer
-----------
-
-.. image:: http://odoo-community.org/logo.png
-   :alt: Odoo Community Association
-   :target: http://odoo-community.org
-
-This module is maintained by the OCA.
-
-OCA, or the Odoo Community Association, is a nonprofit organization whose
-mission is to support the collaborative development of Odoo features and
-promote its widespread use.
-
-To contribute to this module, please visit http://odoo-community.org.
-
-
-    """,
-    "init_xml": [],
-    "update_xml": [
+    "data": [
         "view/purchase_request_view.xml",
         "security/purchase_security.xml",
     ],
-    'demo_xml': [
-
-    ],
-    'test': [
-    ],
+    "license": "AGPL-3",
     'installable': True,
-    'active': False,
-    'certificate': '',
 }
