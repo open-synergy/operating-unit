32794282184f924d03e8a0868f93ebfd3fb8a0de
Andhitia Rama <andhitia.r@gmail.com>
2017-11-04 20:54:23 +0700

    [8.0.2.0.0] account_operating_unit

--- account_operating_unit/models/__init__.py
+++ account_operating_unit/models/__init__.py
@@ -5,5 +5,6 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from . import company
 from . import account_account
+from . import account_journal
 from . import account_move
 from . import invoice
