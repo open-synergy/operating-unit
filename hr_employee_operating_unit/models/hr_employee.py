610c55a825c86ff5746c20c9487c7a6351d3136d
mike <viriyananda.michael@gmail.com>
2017-11-27 10:55:05 +0700

    [8.0.1.0.0][BACKPORT]hr_employee_operating_unit

--- hr_employee_operating_unit/models/hr_employee.py
+++ hr_employee_operating_unit/models/hr_employee.py
@@ -1,24 +1,26 @@
 # -*- coding: utf-8 -*-
 # © 2017 Genweb2 Limited - Matiar Rahman
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from odoo import api, fields, models
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from openerp import api, fields, models
 
 
 class HREmployee(models.Model):
-
     _inherit = 'hr.employee'
 
     @api.model
-    def _get_operating_unit(self):
-        user = self.env['res.users'].browse(self._uid)
+    def operating_unit_default_get(self, uid2):
+        if not uid2:
+            uid2 = self._uid
+        user = self.env['res.users'].browse(uid2)
         return user.default_operating_unit_id
+
+    @api.model
+    def _get_operating_unit(self):
+        return self.operating_unit_default_get(self._uid)
 
     @api.model
     def _get_operating_units(self):
         return self._get_operating_unit()
-
-    company_id = fields.Many2one('res.company', string='Company',
-                                 default=lambda self: self.env.user.company_id)
 
     operating_unit_ids = fields.Many2many('operating.unit',
                                           'operating_unit_emp_rel',
