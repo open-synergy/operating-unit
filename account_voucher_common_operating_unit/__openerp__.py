eb747f8ec5bec9a97154238b5093fed630726ecc
mike <viriyananda.michael@gmail.com>
2018-01-15 16:44:58 +0700

    [8.0.1.0.0][account_voucher_common_operating_unit]

---
+++ account_voucher_common_operating_unit/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Operating Unit in Account Voucher",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Accounting",
+    "depends": [
+        "account_voucher_common",
+        "account_operating_unit"
+    ],
+    "data": [
+        "views/account_voucher_common_views.xml"
+    ],
+    'installable': True,
+}
---
+++ account_voucher_common_operating_unit/models/voucher_common.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from openerp import fields, models, api
+
+
+class AccountVoucherCommon(models.AbstractModel):
+    _inherit = "account.voucher_common"
+
+    operating_unit_id = fields.Many2one(
+        string="Default Operating Unit",
+        comodel_name="operating.unit"
+    )
+
+    @api.onchange(
+        "journal_id")
+    def onchange_ou_id(self):
+        journal = self.journal_id
+        ou = journal.operating_unit_id
+
+        if not ou:
+            ou_user =\
+                self.env['res.users'].operating_unit_default_get(self._uid)
+            self.operating_unit_id = ou_user.id
+        else:
+            self.operating_unit_id = ou.id
+
+    @api.multi
+    def _prepare_account_move(self):
+        res = super(AccountVoucherCommon, self)._prepare_account_move()
+        res["operating_unit_id"] = self.operating_unit_id.id
+        return res
