a0102a792e64e7a13903415668cb168cf2b9e761
mike <viriyananda.michael@gmail.com>
2018-02-13 17:05:17 +0700

    Adding new module stock_picking_wave_operating_unit
    Update Depedencies
    
    Adding rule for stock picking wave type
    
    Revision
    
    Fixing travis error

---
+++ stock_picking_wave_operating_unit/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Picking Wave with Operating Units",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Stock",
+    "depends": [
+        "stock_picking_wave_type",
+        "stock_operating_unit"
+    ],
+    "data": [
+        "security/stock_picking_wave_security.xml",
+        "views/stock_picking_wave_views.xml"
+    ],
+    "installable": True,
+    "auto_install": False,
+}
