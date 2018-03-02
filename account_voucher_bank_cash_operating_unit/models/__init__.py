4aa92504349520cce14d7f79311e4f3c31fc43e8
mike <viriyananda.michael@gmail.com>
2018-03-02 13:10:51 +0700

    [UPD] Added inheritance to account_voucher_common

--- account_voucher_bank_cash_operating_unit/models/__init__.py
+++ account_voucher_bank_cash_operating_unit/models/__init__.py
@@ -2,4 +2,8 @@
 # Copyright 2018 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import models
+from . import (
+    bank_receipt,
+    bank_payment,
+    cash_receipt,
+    cash_payment)
