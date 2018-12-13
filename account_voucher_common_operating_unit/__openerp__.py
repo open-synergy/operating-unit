ac8ea8fb9d703091b4c7ac7e54ded674d409a2be
Andhitia Rama <andhitia.r@gmail.com>
2018-12-13 08:55:23 +0700

    [8.0.1.1.0] account_voucher_common_operating_unit

--- account_voucher_common_operating_unit/__openerp__.py
+++ account_voucher_common_operating_unit/__openerp__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
 
 {
     "name": "Operating Unit in Account Voucher",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "author": "OpenSynergy Indonesia",
     "license": "AGPL-3",
     "website": "https://opensynergy-indonesia.com",
