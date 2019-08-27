35a951e4149b21acc4301addb8aa4a3aba20d50e
mikevhe18 <viriyananda.michael@gmail.com>
2019-08-27 17:21:03 +0700

    [8.0.1.0.0][pos_order_analysis_operating_unit]

---
+++ pos_order_analysis_operating_unit/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "PoS Order Analysis with Operating Units",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Point Of Sale Management",
+    "depends": [
+        "pos_order_analysis",
+        "pos_operating_unit"
+    ],
+    "data": [
+        "security/pos_order_analysis_security.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
